f420047c1ac7fc5965609ea161fdfae3348c17de
Juri Jung <beewee22@gmail.com>
2021-06-20 14:50:03 +0900

    feat(iam-finder): create func, list interfaces
    added IAMFinder struct and constructor, interface metthods in main package

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/beewee22/old-iam-finder
+
+go 1.15
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.6.0
+	github.com/aws/aws-sdk-go-v2/config v1.3.0
+	github.com/aws/aws-sdk-go-v2/service/iam v1.5.1
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.10.0
+)
---
+++ iam-finder.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/iam/types"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
+)
+
+type IAMFinder struct {
+	iam *iam.Client
+}
+
+//NewIAMFinderFromAWSConfig Create new IAMFinder from config
+func NewIAMFinderFromAWSConfig(cfg aws.Config) *IAMFinder {
+	IAMClient := &IAMFinder{
+		iam: iam.NewFromConfig(cfg),
+	}
+
+	return IAMClient
+}
+
+//ListAllIAMUsers List all IAM users from account
+func (f IAMFinder) ListAllIAMUsers() *iam.ListUsersOutput {
+	userList, err := f.iam.ListUsers(context.TODO(), &iam.ListUsersInput{})
+
+	if err != nil {
+		fmt.Println("Got an error retrieving users:")
+		panic(err)
+	}
+
+	return userList
+}
+
+//ListAllAccessKeyMetadata List all IAM access key metadata
+func (f IAMFinder) ListAllAccessKeyMetadata() (accessKeys []types.AccessKeyMetadata) {
+	IAMList := f.ListAllIAMUsers()
+
+	for _, user := range IAMList.Users {
+		accessKey, err := f.iam.ListAccessKeys(
+			context.TODO(),
+			&iam.ListAccessKeysInput{UserName: user.UserName},
+		)
+		if err != nil {
+			fmt.Println("Got an error retrieving access key from user" + *user.UserName)
+			fmt.Printf("%v", err)
+			continue
+		}
+		if accessKey != nil {
+			accessKeys = append(accessKeys, accessKey.AccessKeyMetadata...)
+		}
+	}
+	return
+}
+
+func (f IAMFinder) ListAllOldAccessKey(expireHours int) (oldAccessKeyList []types.AccessKeyMetadata) {
+	// calc expected at least time
+	expireDuration := time.Duration(expireHours) * time.Hour * -1
+	expectedLastUsed := time.Now().Add(expireDuration)
+
+	accessKeyMetadataList := f.ListAllAccessKeyMetadata()
+
+	for _, accessKeyMetadata := range accessKeyMetadataList {
+		last, err := f.iam.GetAccessKeyLastUsed(context.TODO(), &iam.GetAccessKeyLastUsedInput{
+			AccessKeyId: accessKeyMetadata.AccessKeyId,
+		})
+		if err != nil {
+			fmt.Println("Got an error retrieving access key last used time:")
+			fmt.Printf("%v", accessKeyMetadata)
+		}
+		if last != nil {
+			var lastUsedTime *time.Time
+			// check if access key was not used
+			if last.AccessKeyLastUsed.LastUsedDate == nil {
+				// if the access key was not used at all, check with created date
+				lastUsedTime = accessKeyMetadata.CreateDate
+			} else {
+				lastUsedTime = last.AccessKeyLastUsed.LastUsedDate
+			}
+			// check last used before expected time
+			if lastUsedTime.Before(expectedLastUsed) {
+				oldAccessKeyList = append(oldAccessKeyList, accessKeyMetadata)
+			}
+		}
+	}
+
+	return
+}
+
