2420cfd70c9ea1cd9dc6aaa35fc8914315fec2c7
Juri Jung <beewee22@gmail.com>
2021-06-20 17:36:13 +0900

    refactor(iam-finder): refactor code for test logics
    split some codes for function tests
    
    TODO:
      [X] send expired accesskey to slack channel
      [X] receive ENVs for AWS credential, slack webhook
      [] add test code
      [] write Dockerfile
      [] add k8s yaml
      [] update README.md

--- iam-finder.go
+++ iam-finder.go
@@ -59,19 +59,17 @@ func (f IAMFinder) ListAllAccessKeyMetadata() (accessKeys []types.AccessKeyMetad
 }
 
 type OldAccessKeyInfo struct {
-	UserName string
+	UserName    string
 	AccessKeyId string
-	LastUsed time.Time
-	IsNotUsed bool
+	LastUsed    time.Time
+	IsNotUsed   bool
 }
 
 const AccesskeyPrefix = "AKIA"
 
+//ListAllOldAccessKey list old access keys from all access keys starts with "AKIA" prefix
 func (f IAMFinder) ListAllOldAccessKey(expireHours int) (oldAccessKeyList []OldAccessKeyInfo) {
-	// calc expected at least time
-	expireDuration := time.Duration(expireHours) * time.Hour * -1
-	expectedLastUsed := time.Now().Add(expireDuration)
-
+	// get metadata list
 	accessKeyMetadataList := f.ListAllAccessKeyMetadata()
 
 	for _, accessKeyMetadata := range accessKeyMetadataList {
@@ -79,6 +77,8 @@ func (f IAMFinder) ListAllOldAccessKey(expireHours int) (oldAccessKeyList []OldA
 		if !strings.HasPrefix(*accessKeyMetadata.AccessKeyId, AccesskeyPrefix) {
 			continue
 		}
+
+		// get last used time by access key
 		last, err := f.iam.GetAccessKeyLastUsed(context.TODO(), &iam.GetAccessKeyLastUsedInput{
 			AccessKeyId: accessKeyMetadata.AccessKeyId,
 		})
@@ -86,24 +86,16 @@ func (f IAMFinder) ListAllOldAccessKey(expireHours int) (oldAccessKeyList []OldA
 			fmt.Println("Got an error retrieving access key last used time:")
 			fmt.Printf("%v", accessKeyMetadata)
 		}
+
 		if last != nil {
-			var lastUsedTime *time.Time
-			isNotUsed := false
-			// check if access key was not used
-			if last.AccessKeyLastUsed.LastUsedDate == nil {
-				// if the access key was not used at all, check with created date
-				lastUsedTime = accessKeyMetadata.CreateDate
-				isNotUsed = true
-			} else {
-				lastUsedTime = last.AccessKeyLastUsed.LastUsedDate
-			}
+			lastUsedTime, isNotUsed := getLastUsedOrCreated(last.AccessKeyLastUsed.LastUsedDate, accessKeyMetadata.CreateDate)
 			// check last used before expected time
-			if lastUsedTime.Before(expectedLastUsed) {
+			if isAccessKeyOld(lastUsedTime, expireHours) {
 				oldAccessKeyList = append(oldAccessKeyList, OldAccessKeyInfo{
-					UserName:  *last.UserName,
+					UserName:    *last.UserName,
 					AccessKeyId: *accessKeyMetadata.AccessKeyId,
-					LastUsed: *lastUsedTime,
-					IsNotUsed: isNotUsed,
+					LastUsed:    *lastUsedTime,
+					IsNotUsed:   isNotUsed,
 				})
 			}
 		}
@@ -112,3 +104,23 @@ func (f IAMFinder) ListAllOldAccessKey(expireHours int) (oldAccessKeyList []OldA
 	return
 }
 
+func getLastUsedOrCreated(lastUsed *time.Time, created *time.Time) (lastTouched *time.Time, isNotUsed bool) {
+	// check if access key was not used
+	if lastUsed == nil {
+		// if the access key was not used at all, check with created date
+		lastTouched = created
+		isNotUsed = true
+	} else {
+		lastTouched = lastUsed
+		isNotUsed = false
+	}
+
+	return
+}
+
+func isAccessKeyOld(lastUsed *time.Time, expireHours int) bool {
+	// calc expected at least time
+	expireDuration := time.Duration(expireHours) * time.Hour * -1
+	expectedLastUsed := time.Now().Add(expireDuration)
+	return lastUsed.Before(expectedLastUsed)
+}
